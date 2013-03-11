1ec5f78acc4d0f50a8e6b872d87c5c4eaf5bae5e
mbax <matt@phozop.net>
2013-03-11 17:57:13 -0400

    Formatting, cleanup. Now happy

--- src/main/java/team/bukkthat/commands/HomeCommand.java
+++ src/main/java/team/bukkthat/commands/HomeCommand.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,59 +17,68 @@ import team.bukkthat.Main;
 
 public class HomeCommand implements CommandExecutor {
 
-	Main plugin;
-	YamlConfiguration conf;
-	File f;
-	
-	public HomeCommand(Main m) {
-		this.plugin = m;
-		f = new File(plugin.getDataFolder(), "homes.yml");
-		if(!f.exists()) {
+    private final Main plugin;
+    private final YamlConfiguration conf;
+    private final File confFile;
+
+    public HomeCommand(Main m) {
+        this.plugin = m;
+        this.confFile = new File(this.plugin.getDataFolder(), "homes.yml");
+        if (!this.confFile.exists()) {
             try {
-                f.createNewFile();
-            } catch (IOException e) {
+                this.confFile.createNewFile();
+            } catch (final IOException e) {
                 e.printStackTrace();
             }
         }
-		this.conf=YamlConfiguration.loadConfiguration(f);
-	}
+        this.conf = YamlConfiguration.loadConfiguration(this.confFile);
+    }
 
-	public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
-		if(cs instanceof Player) {
-			Player p = (Player) cs;
-			if(cmd.getName().equalsIgnoreCase("home")) {
-				if(conf.contains(p.getName())) {
-                    p.sendMessage(ChatColor.RED+"No Home Set!");
+    public boolean onCommand(CommandSender cs, Command cmd, String label, String[] args) {
+        if (cs instanceof Player) {
+            final Player p = (Player) cs;
+            if (cmd.getName().equalsIgnoreCase("home")) {
+                if (this.conf.contains(p.getName())) {
+                    p.sendMessage(ChatColor.RED + "No Home Set!");
                     return true;
                 }
-				String world = this.conf.getString(p.getName()+".world");
-				double x = this.conf.getDouble(p.getName()+".x");
-				double y = this.conf.getDouble(p.getName()+".y");
-				double z = this.conf.getDouble(p.getName()+".z");
-				Location l = new Location(Bukkit.getWorld(world), x, y, z);
-				p.teleport(l);
-				return true;
-			}
-			else if(cmd.getName().equalsIgnoreCase("sethome")){
-				Location l = p.getLocation();
-				this.conf.set(p.getName()+".world", l.getWorld().getName());
-				this.conf.set(p.getName()+".x", l.getBlockX());
-				this.conf.set(p.getName()+".y", l.getBlockY());
-				this.conf.set(p.getName()+".z", l.getBlockZ());
-				try {
-					this.conf.save(f);
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-				p.sendMessage(ChatColor.GREEN+"Home Set!");
-				return true;
-			}
-		}
-		else {
-			cs.sendMessage(ChatColor.RED+"Only a player can do that!");
-			return true;
-		}
-		return true;
-	}
-	
+                final String playerName = p.getName();
+                final String worldName = this.conf.getString(playerName + ".world");
+                final double x = this.conf.getDouble(playerName + ".x");
+                final double y = this.conf.getDouble(playerName + ".y");
+                final double z = this.conf.getDouble(playerName + ".z");
+                World world = Bukkit.getWorld(worldName);
+                if (world != null) {
+                    p.teleport(new Location(world, x, y, z));
+                } else {
+                    this.conf.set(playerName, null);
+                    this.saveHomes();
+                    p.sendMessage(ChatColor.RED + "Your home no longer exists!");
+                }
+                return true;
+            } else if (cmd.getName().equalsIgnoreCase("sethome")) {
+                final Location l = p.getLocation();
+                this.conf.set(p.getName() + ".world", l.getWorld().getName());
+                this.conf.set(p.getName() + ".x", l.getBlockX());
+                this.conf.set(p.getName() + ".y", l.getBlockY());
+                this.conf.set(p.getName() + ".z", l.getBlockZ());
+                this.saveHomes();
+                p.sendMessage(ChatColor.GREEN + "Home Set!");
+                return true;
+            }
+        } else {
+            cs.sendMessage(ChatColor.RED + "Only a player can do that!");
+            return true;
+        }
+        return true;
+    }
+
+    private void saveHomes() {
+        try {
+            this.conf.save(this.confFile);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
